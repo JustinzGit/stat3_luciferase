d0e39be355101d521bfa18cf96d1dff4a5ff4313
Justin <justinms@outlook.com>
2020-12-03 21:41:14 -0600

    load variants into database

--- stat3_luciferase_api/db/seeds.rb
+++ stat3_luciferase_api/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+variants = CSV.parse(File.read('./db/data/variant.csv'), headers: :first_row)
+
+variants.each do |v|
+    Variant.create(protein_variant: v['protein_variant'], wt_amino_acid: v['wt_amino_acid'], 
+        mt_amino_acid: v['mt_amino_acid'], aa_position: v['aa_position'])
+end 
