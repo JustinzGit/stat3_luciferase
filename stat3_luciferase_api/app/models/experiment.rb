c2f27facfc753ae42c4879a8dfa852fb941c6e5f
Justin <justinms@outlook.com>
2020-12-03 21:55:08 -0600

    correct syntax error

--- stat3_luciferase_api/app/models/experiment.rb
+++ stat3_luciferase_api/app/models/experiment.rb
@@ -1,4 +1,4 @@
 class Experiment < ApplicationRecord
     has_many :luciferase_values
-    has_many :variants through :luciferase_values
+    has_many :variants through: :luciferase_values
 end
--- stat3_luciferase_api/app/models/variant.rb
+++ stat3_luciferase_api/app/models/variant.rb
@@ -1,4 +1,4 @@
 class Variant < ApplicationRecord
     has_many :luciferase_values
-    has_many :experiments through :luciferase_values
+    has_many :experiments through: :luciferase_values
 end
