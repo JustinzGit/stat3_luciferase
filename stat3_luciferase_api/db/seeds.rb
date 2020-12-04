69026d769a76c66f91d519446e7ce34f095d405b
Justin <justinms@outlook.com>
2020-12-03 21:46:51 -0600

    load luciferase values into db

--- stat3_luciferase_api/db/seeds.rb
+++ stat3_luciferase_api/db/seeds.rb
@@ -6,3 +6,11 @@ variants.each do |v|
     Variant.create(protein_variant: v['protein_variant'], wt_amino_acid: v['wt_amino_acid'], 
         mt_amino_acid: v['mt_amino_acid'], aa_position: v['aa_position'])
 end 
+
+
+luciferase_values = CSV.parse(File.read('./db/data/luciferase_values.csv'), headers: :first_row)
+
+luciferase_values.each do |lv|
+    LuciferaseValue.create(firefly: lv['firefly'], renilla: lv['renilla'], 
+        variant_id: lv['variant_id'], experiment_id: lv['experiment_id'])
+end
