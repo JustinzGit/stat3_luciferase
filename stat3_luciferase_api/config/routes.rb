c16110219271390df4692389c1d8709f5ab54db6
Justin <justinms@outlook.com>
2020-12-08 19:26:45 -0600

    only allow variant index and show action

--- stat3_luciferase_api/config/routes.rb
+++ stat3_luciferase_api/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :experiments
   resources :luciferase_values
-  resources :variants
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :variants, only: [:index]
+  get '/variants/:protein_variant', to: 'variants#show'
 end
