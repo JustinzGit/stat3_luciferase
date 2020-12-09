0ab0366a0110ad3c75cadf3e7da93df10363d02e
Justin <justinms@outlook.com>
2020-12-08 20:38:10 -0600

    remove luciferase_values routes

--- stat3_luciferase_api/config/routes.rb
+++ stat3_luciferase_api/config/routes.rb
@@ -1,6 +1,4 @@
-Rails.application.routes.draw do
-  resources :luciferase_values
-  
+Rails.application.routes.draw do  
   resources :experiments, only: [:index]
   get '/experiments/:date', to: 'experiments#show'
 
