ab04b668447ddc07b9a857aac4731c405c471e5e
Justin <justinms@outlook.com>
2020-12-08 19:28:08 -0600

    minor syntax changes

--- stat3_luciferase_api/app/serializers/variant_experiment_serializer.rb
+++ stat3_luciferase_api/app/serializers/variant_experiment_serializer.rb
@@ -1,6 +1,5 @@
 class VariantExperimentSerializer < ActiveModel::Serializer
-  attributes :date, :wt_firefly, :wt_renilla
-  attribute :luciferase_values
+  attributes :date, :wt_firefly, :wt_renilla, :luciferase_values
 
   def luciferase_values
     luciferase_value = object.luciferase_values.where(variant_id: @instance_options[:variant].id).map do |luciferase_value|
