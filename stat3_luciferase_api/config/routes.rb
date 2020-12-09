11972c3b49a47465fd9e2c9d96aef27c217fc4e6
Justin <justinms@outlook.com>
2020-12-08 20:20:08 -0600

    only allow experiment index and show action

--- stat3_luciferase_api/config/routes.rb
+++ stat3_luciferase_api/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :experiments
   resources :luciferase_values
   
+  resources :experiments, only: [:index]
+  get '/experiments/:date', to: 'experiments#show'
+
   resources :variants, only: [:index]
   get '/variants/:protein_variant', to: 'variants#show'
 end
