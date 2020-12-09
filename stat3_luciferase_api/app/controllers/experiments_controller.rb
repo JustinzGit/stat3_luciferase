acd6daec3e64d9d8c7f5aee83fa779336bc7244a
Justin <justinms@outlook.com>
2020-12-08 20:10:12 -0600

    order experiments by date

--- stat3_luciferase_api/app/controllers/experiments_controller.rb
+++ stat3_luciferase_api/app/controllers/experiments_controller.rb
@@ -3,40 +3,40 @@ class ExperimentsController < ApplicationController
 
   # GET /experiments
   def index
-    @experiments = Experiment.all
+    @experiments = Experiment.all.order(date: :desc)
 
     render json: @experiments
   end
 
-  # GET /experiments/1
-  def show
-    render json: @experiment
-  end
+  # # GET /experiments/1
+  # def show
+  #   render json: @experiment
+  # end
 
-  # POST /experiments
-  def create
-    @experiment = Experiment.new(experiment_params)
+  # # POST /experiments
+  # def create
+  #   @experiment = Experiment.new(experiment_params)
 
-    if @experiment.save
-      render json: @experiment, status: :created, location: @experiment
-    else
-      render json: @experiment.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @experiment.save
+  #     render json: @experiment, status: :created, location: @experiment
+  #   else
+  #     render json: @experiment.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # PATCH/PUT /experiments/1
-  def update
-    if @experiment.update(experiment_params)
-      render json: @experiment
-    else
-      render json: @experiment.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /experiments/1
+  # def update
+  #   if @experiment.update(experiment_params)
+  #     render json: @experiment
+  #   else
+  #     render json: @experiment.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # DELETE /experiments/1
-  def destroy
-    @experiment.destroy
-  end
+  # # DELETE /experiments/1
+  # def destroy
+  #   @experiment.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
