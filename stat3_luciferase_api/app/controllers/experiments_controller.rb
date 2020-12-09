a14b0d30d898544479641dd9c8abcae8a7c9e09a
Justin <justinms@outlook.com>
2020-12-08 20:11:55 -0600

    remove callback

--- stat3_luciferase_api/app/controllers/experiments_controller.rb
+++ stat3_luciferase_api/app/controllers/experiments_controller.rb
@@ -1,5 +1,4 @@
 class ExperimentsController < ApplicationController
-  before_action :set_experiment, only: [:show, :update, :destroy]
 
   # GET /experiments
   def index
@@ -39,11 +38,6 @@ class ExperimentsController < ApplicationController
   # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_experiment
-      @experiment = Experiment.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def experiment_params
       params.require(:experiment).permit(:date, :wt_firefly, :wt_renilla)
