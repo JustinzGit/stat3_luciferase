7f88bbae09ebd0cdd256c186ec44ab76370f178b
Justin <justinms@outlook.com>
2020-12-12 10:19:02 -0600

    setup route to variant component

--- stat3_luciferase_client/src/App.js
+++ stat3_luciferase_client/src/App.js
@@ -1,12 +1,17 @@
 import { Switch, Route } from 'react-router-dom'
 
 import VariantList from './components/VariantList'
+import Variant from './components/Variant'
 
 function App() {
   return (
     <Switch>
+      <Route path={'/variants/:protein_variant'}>
+        <Variant />
+      </Route>
+
       <Route path={'/variants'}>
-          <VariantList />
+        <VariantList />
       </Route>
     </Switch>
   );
