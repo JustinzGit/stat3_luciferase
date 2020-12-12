6bd1ad47aa450fc3aba7f68ecbf90cf3f911f648
Justin <justinms@outlook.com>
2020-12-12 10:18:49 -0600

    create Variant component

---
+++ stat3_luciferase_client/src/components/Variant.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+class Variant extends Component {
+    state = {
+        variantData: []
+    }
+
+    render(){
+        return(
+            <div>
+                Hello World
+            </div>
+        )
+    }
+}
+
+export default Variant
