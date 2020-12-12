7022eaa1f0b8993fcc0aca4bf5edd8621db54e05
Justin <justinms@outlook.com>
2020-12-12 09:23:05 -0600

    remove experiments attribute, add id and aa position

--- stat3_luciferase_api/app/serializers/variant_serializer.rb
+++ stat3_luciferase_api/app/serializers/variant_serializer.rb
@@ -1,11 +1,11 @@
 class VariantSerializer < ActiveModel::Serializer
-  attributes :protein_variant, :avg_fold_change, :gof, :experiments
+  attributes :id, :gof, :protein_variant, :avg_fold_change, :aa_position
 
-  def experiments
-    object.experiments.map do |experiment|
-      VariantExperimentSerializer.new(experiment, variant: object)
-    end
-  end
+  # def experiments
+  #   object.experiments.map do |experiment|
+  #     VariantExperimentSerializer.new(experiment, variant: object)
+  #   end
+  # end
 
   def avg_fold_change
     "#{'%.2f' % object.avg_fold_change}"
