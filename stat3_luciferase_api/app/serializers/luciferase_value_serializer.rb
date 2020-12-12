74ff0b8bbc3b5c3b09c5bce0f4d7f9eacfd9494b
Justin <justinms@outlook.com>
2020-12-12 15:52:29 -0600

    format values

--- stat3_luciferase_api/app/serializers/luciferase_value_serializer.rb
+++ stat3_luciferase_api/app/serializers/luciferase_value_serializer.rb
@@ -4,5 +4,13 @@ class LuciferaseValueSerializer < ActiveModel::Serializer
   def variant
     variant = Variant.find(self.object.variant_id)
     variant.protein_variant
+  end
+
+  def ff_ren_ratio
+    "#{'%.2f' % object.ff_ren_ratio}".to_f
+  end 
+
+  def fold_change
+    "#{'%.2f' % object.fold_change}".to_f
   end 
 end
