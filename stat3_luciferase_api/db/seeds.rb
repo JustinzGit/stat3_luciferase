d42ec791062ba37d177f7d69ea2a6d57b644c248
Justin <justinms@outlook.com>
2020-12-11 20:52:28 -0600

    calculate ratios and foldchange

--- stat3_luciferase_api/db/seeds.rb
+++ stat3_luciferase_api/db/seeds.rb
@@ -12,12 +12,26 @@ experiments = CSV.parse(File.read('./db/data/experiments.csv'), headers: :first_
 experiments.each do |e|
     date = e['date'].split(".")
     date = "#{date[2]}-#{date[0]}-#{date[1]}"
-    Experiment.create(date: date, wt_firefly: e['wt_firefly'], wt_renilla: e['wt_renilla'])
+
+    wt_ff = e['wt_firefly'].to_f
+    wt_ren = e['wt_renilla'].to_f
+    ff_ren_ratio = wt_ff/wt_ren
+
+    Experiment.create(date: date, wt_firefly: wt_ff, wt_renilla: wt_ren, ff_ren_ratio: ff_ren_ratio )
 end
 
 luciferase_values = CSV.parse(File.read('./db/data/luciferase_values.csv'), headers: :first_row)
 
 luciferase_values.each do |lv|
-    LuciferaseValue.create(firefly: lv['firefly'], renilla: lv['renilla'], 
-        variant_id: lv['variant_id'], experiment_id: lv['experiment_id'])
+    ff = lv['firefly'].to_f
+    ren = lv['firefly'].to_f
+    ff_ren_ratio = ff/ren
+
+    experiment = Experiment.find(lv['experiment_id'])
+    fold_change = ff_ren_ratio/experiment.ff_ren_ratio
+
+    fold_change > 1 ? gof = true : gof = false
+
+    LuciferaseValue.create(firefly: ff, renilla: ren, ff_ren_ratio: ff_ren_ratio, fold_change: fold_change, 
+        gof: gof, variant_id: lv['variant_id'], experiment_id: lv['experiment_id'])
 end
