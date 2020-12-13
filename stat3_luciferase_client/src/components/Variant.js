6179c981fc4227a938203a3a50eafb0e71fba1e2
Justin <justinms@outlook.com>
2020-12-12 20:08:14 -0600

    render variant experiments

--- stat3_luciferase_client/src/components/Variant.js
+++ stat3_luciferase_client/src/components/Variant.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react"
 
 import { useParams } from 'react-router-dom'
 
+import Experiment from './Experiment'
+
 function Variant() {
     const { protein_variant } = useParams()
-    const [variant, setVariant] = useState({})
+    const [variant, setVariant] = useState({ experiments: [] })
 
     useEffect(() => {
         fetch(`http://localhost:3001/variants/${protein_variant}`, { credentials: 'include' })
@@ -19,6 +21,7 @@ function Variant() {
            <p>Protien Variant: {variant.protein_variant}</p>
            <p>Average Fold Change: {variant.avg_fold_change}</p>
            <p>GOF? {variant.gof ? "Yes" : "No"}</p>
+           {variant.experiments.map((data) => <Experiment data={data}/>)}
         </div>
     )
 }
