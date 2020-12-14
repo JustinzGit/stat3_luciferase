391a794443768c0c20852b8622e88eac03c4112e
Justin <justinms@outlook.com>
2020-12-13 19:30:59 -0600

    add datalist with variant options

--- stat3_luciferase_client/src/components/Navigation.js
+++ stat3_luciferase_client/src/components/Navigation.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 
-function Navigation() {
+function Navigation({ variants }) {
+
     return(
         <div id="navigation">
             <NavLink to="/variants">Variant Table</NavLink>
+
+            <input list="variants" id="variant-selection"/>
+            <datalist id="variants">
+                {variants.map((variant) => <option value={variant.protein_variant}>{variant.protein_variant}</option>)}
+            </datalist>
         </div>
     )
 }
