e356e840ff0a83da24ee3f64e4b663edd3b2f222
Justin <justinms@outlook.com>
2020-12-13 21:14:18 -0600

    allow redirection on variant selection

--- stat3_luciferase_client/src/components/Navigation.js
+++ stat3_luciferase_client/src/components/Navigation.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom'
 
 function Navigation({ variants }) {
+    const [selectedVariant, setSelectedVariant] = useState("")
+    const history = useHistory()
+
+    function goToVariant(event){
+        event.preventDefault()
+        history.push(`/variants/${selectedVariant}`)
+    }
 
     return(
         <div id="navigation">
             <NavLink to="/variants">Variant Table</NavLink>
 
-            <input list="variants" id="variant-selection"/>
-            <datalist id="variants">
-                {variants.map((variant) => <option value={variant.protein_variant}>{variant.protein_variant}</option>)}
-            </datalist>
+            <form onSubmit={goToVariant}>
+                <input onChange={event => setSelectedVariant(event.target.value)} list="variants" id="variant-selection" autoComplete="off" />
+                <datalist id="variants">
+                    {variants.map((variant) => <option key={variant.id} value={variant.protein_variant}>{variant.protein_variant}</option>)}
+                </datalist>
+                <input type="submit" value="Go To Variant"/>
+            </form>
         </div>
     )
 }
