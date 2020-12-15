eb2fbd7fb1d42c373df5c8951fea01c64a59a51d
Justin <justinms@outlook.com>
2020-12-14 21:58:57 -0600

    set todays date after render

--- stat3_luciferase_client/src/components/ExperimentForm.js
+++ stat3_luciferase_client/src/components/ExperimentForm.js
@@ -1,9 +1,22 @@
+import { useState, useEffect } from 'react'
+
 function ExperimentForm(){
+    const [date, setDate] = useState()
+   
+    useEffect(() => {
+        let dateObj = new Date()
+        let day = String(dateObj.getDate()).padStart(2, '0')
+        let month = String(dateObj.getMonth() + 1).padStart(2, '0')
+        let year = dateObj.getFullYear();
+        let todaysDate = year + '-' + month + '-' + day
+        setDate(todaysDate)
+    }, [])
+
     return(
         <div>
             <form>
                 <h3>Add Experiment</h3>
-                <p>Date: <input type="date"/></p>
+                <p>Date: <input value={date} type="date"/></p>
             
                 <p>WT Firefly: <input type="number"/></p>
                 <p>WT Renilla: <input type="number"/></p>
