e5049a2c39568e2510a0152147251d1deb8c82e0
Justin <justinms@outlook.com>
2020-12-16 21:52:34 -0600

    set wt values on form change

--- stat3_luciferase_client/src/components/ExperimentForm.js
+++ stat3_luciferase_client/src/components/ExperimentForm.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 
 function ExperimentForm(){
     const [date, setDate] = useState()
+    const [luciferaseValues, setLuciferaseValues] = useState({})
    
     useEffect(() => {
         let dateObj = new Date()
@@ -12,14 +13,26 @@ function ExperimentForm(){
         setDate(todaysDate)
     }, [])
 
+    function handleFormChange(event){
+        const plasmidEntry = event.target.name
+        const luciferaseValue = event.target.value
+        setLuciferaseValues({
+            ...luciferaseValues, 
+            [plasmidEntry]: luciferaseValue
+        })
+
+    }
+
     return(
         <div>
             <form>
                 <h3>Add Experiment</h3>
                 <p>Date: <input value={date} type="date"/></p>
             
-                <p>WT Firefly: <input type="number"/></p>
-                <p>WT Renilla: <input type="number"/></p>
+                <p>WT Firefly: <input onChange={handleFormChange} type="number" name="wt_firefly" /></p>
+                <p>WT Renilla: <input onChange={handleFormChange} type="number" name="wt_renilla" /></p>
+                <p>Firefly: {luciferaseValues.wt_firefly}</p>
+                <p>Renilla: {luciferaseValues.wt_renilla}</p>
 
                 <input type="submit" />
             </form>
