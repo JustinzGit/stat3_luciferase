d12bed5f881807aad7bae4902d2143f9de5d754c
Justin <justinms@outlook.com>
2020-12-26 10:40:07 -0600

    add model validations

--- stat3_luciferase_api/app/models/experiment.rb
+++ stat3_luciferase_api/app/models/experiment.rb
@@ -1,4 +1,7 @@
 class Experiment < ApplicationRecord
+    validates :date, :wt_firefly, :wt_renilla, presence: true
+    validates :date, uniqueness: true
+
     has_many :luciferase_values
     has_many :variants, through: :luciferase_values
 end
