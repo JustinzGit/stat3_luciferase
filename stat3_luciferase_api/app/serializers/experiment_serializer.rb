bfaf4d58e891bd7000d8ec9214933f2321a52b64
Justin <justinms@outlook.com>
2020-12-26 10:41:07 -0600

    add method to format additional attribute

--- stat3_luciferase_api/app/serializers/experiment_serializer.rb
+++ stat3_luciferase_api/app/serializers/experiment_serializer.rb
@@ -1,6 +1,10 @@
 class ExperimentSerializer < ActiveModel::Serializer
-  attributes :date, :wt_firefly, :wt_renilla, :variants
+  attributes :date, :wt_firefly, :wt_renilla, :ff_ren_ratio, :variants
 
+  def ff_ren_ratio
+    "#{'%.2f' % object.ff_ren_ratio}".to_f
+  end 
+  
   def variants
     self.object.variants.map do |variant|
       ExperimentVariantSerializer.new(variant, experiment: object)
