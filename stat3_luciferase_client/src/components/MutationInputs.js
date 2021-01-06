b24816070eb67ed3a1d37015a1a2391d2b4aba70
Justin <justinms@outlook.com>
2021-01-05 18:59:24 -0600

    change name of prop

--- stat3_luciferase_client/src/components/MutationInputs.js
+++ stat3_luciferase_client/src/components/MutationInputs.js
@@ -1,11 +1,11 @@
-function MutationInputs({ index, mtValues, handleMtChange, variantList }){
+function MutationInputs({ index, luciferaseValues, handleMtChange, variantList }){
     return (
         <div key={`mutation_${index}`} id={`mutation_${index}`}>               
             Mutation:<br/>
             <input 
                 list="variants" 
-                data-index={index} 
-                value={mtValues[index].mutation} 
+                data-index={index}
+                value={luciferaseValues[index].protein_variant}  
                 onChange={handleMtChange} 
                 className="protein_variant" 
                 autoComplete="off" />
@@ -22,16 +22,16 @@ function MutationInputs({ index, mtValues, handleMtChange, variantList }){
                 type="number" 
                 data-index={index} 
                 className="firefly"
-                onChange={handleMtChange} 
-                value={mtValues[index].firefly} /></p>
+                onChange={handleMtChange}
+                value={luciferaseValues[index].firefly} /></p>
 
             <p>Renilla: 
             <input 
                 type="number" 
                 data-index={index}
                 className="renilla"
-                onChange={handleMtChange} 
-                value={mtValues[index].renilla} /></p>
+                onChange={handleMtChange}
+                value={luciferaseValues[index].renilla} /></p>
         </div>
     )
 
