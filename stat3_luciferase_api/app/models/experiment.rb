680a02a3554ffbc637f975b4c25583d2422dbbd0
Justin <justinms@outlook.com>
2021-01-12 15:06:58 -0600

    add method that merges calculations

--- stat3_luciferase_api/app/models/experiment.rb
+++ stat3_luciferase_api/app/models/experiment.rb
@@ -5,19 +5,30 @@ class Experiment < ApplicationRecord
     has_many :luciferase_values
     has_many :variants, through: :luciferase_values
 
+    # Sets WT firefly / renilla ratio
     def calculate_ff_ren_ratio
         self.ff_ren_ratio = self.wt_firefly/self.wt_renilla
     end 
 
+    # Iterates through experiments luciferase values 
+    # Sets Firefly / Renilla ratio for each variant
+    # Calculates the fold change for each variant
     def calculate_fold_changes
         self.luciferase_values.each do |lv|
             lv.ff_ren_ratio = lv.firefly/lv.renilla
             lv.fold_change = lv.ff_ren_ratio/self.ff_ren_ratio
         end 
+    end
+    
+    def set_ratios_and_fold_changes
+        self.calculate_ff_ren_ratio
+        self.calculate_fold_changes
     end 
 
-    def set_variant_ids(lv_value_params)
-        lv_value_params.map { |variant_entry|
+    # Iterates through luciferase values
+    # Sets the variant ID for each entry 
+    def set_variant_ids(lv_params)
+        lv_params.map { |variant_entry|
             variant = Variant.find_by(protein_variant: variant_entry[:protein_variant])
             if variant
                 variant_entry[:variant_id] = variant.id
