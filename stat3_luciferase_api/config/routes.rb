aba2f7c59f7c3205a7636b37d9ead220d21c25fb
Justin <justinms@outlook.com>
2021-01-12 19:28:01 -0600

    specify name of route for route helper

--- stat3_luciferase_api/config/routes.rb
+++ stat3_luciferase_api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do  
   resources :experiments, only: [:index, :create]
-  get '/experiments/:date', to: 'experiments#show'
+  get '/experiments/:date', to: 'experiments#show', as: 'experiment'
 
   resources :variants, only: [:index]
   get '/variants/:protein_variant', to: 'variants#show'
