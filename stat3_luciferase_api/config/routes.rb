d55efd63ed9f4e4de006046931f56f0664a9b471
Justin <justinms@outlook.com>
2021-01-13 11:48:07 -0600

    add route to update

--- stat3_luciferase_api/config/routes.rb
+++ stat3_luciferase_api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do  
-  resources :experiments, only: [:index, :create]
-  get '/experiments/:date', to: 'experiments#show', as: 'experiment'
+  resources :experiments, only: [:index, :create, :show, :update]
+  # get '/experiments/date/:date', to: 'experiments#show'
 
   resources :variants, only: [:index]
   get '/variants/:protein_variant', to: 'variants#show'
