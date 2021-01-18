9c5f8fe5f6c91fe7714c5a9a03c02795cd32bf16
Justin <justinms@outlook.com>
2021-01-17 19:07:54 -0600

    include additional attributes

--- stat3_luciferase_client/src/hooks/useExperiment.js
+++ stat3_luciferase_client/src/hooks/useExperiment.js
@@ -23,7 +23,9 @@ function useExperiment(id = null){
                     protein_variant: variant.protein_variant,
                     variant_id: variant.id,
                     firefly: variant.luciferase_values.firefly,
-                    renilla: variant.luciferase_values.renilla
+                    renilla: variant.luciferase_values.renilla,
+                    ff_ren_ratio: variant.luciferase_values.ff_ren_ratio,
+                    fold_change: variant.luciferase_values.fold_change
                 }))
     
                 const experiment = {
@@ -31,6 +33,7 @@ function useExperiment(id = null){
                     date: apiData.date,
                     wt_firefly: apiData.wt_firefly,
                     wt_renilla: apiData.wt_renilla,
+                    ff_ren_ratio: apiData.ff_ren_ratio,
                     luciferase_values: luciferase_values
                 }
     
