951492054b184e835f9370d2b079a0406399c70e
Justin <justinms@outlook.com>
2021-01-17 19:06:20 -0600

    remove uneeded condition

--- stat3_luciferase_client/src/components/Errors.js
+++ stat3_luciferase_client/src/components/Errors.js
@@ -1,8 +1,8 @@
 export default function Errors({ messages }){
     return(
         <div>
-            {messages.length !== 0 && <h2 style={{color: 'red'}}>Error!</h2>}
-            {messages.map(message => <p><b>{message}</b></p>)}
+            {<h2 style={{color: 'red'}}>Error!</h2>}
+            {messages.map((message, index) => <p key={index}><b>{message}</b></p>)}
         </div>
     )
 }
