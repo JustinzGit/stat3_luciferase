d245bd4d5bd5957059906d948d3d94d01f9aeb5d
Justin <justinms@outlook.com>
2021-01-22 14:34:28 -0600

    remove uneeded functions

--- stat3_luciferase_api/app/models/experiment.rb
+++ stat3_luciferase_api/app/models/experiment.rb
@@ -6,24 +6,4 @@ class Experiment < ApplicationRecord
     has_many :variants, through: :luciferase_values
 
     accepts_nested_attributes_for :luciferase_values
-
-    # Sets WT firefly / renilla ratio
-    def calculate_ff_ren_ratio
-        self.ff_ren_ratio = self.wt_firefly/self.wt_renilla
-    end 
-
-    # Iterates through experiments luciferase values 
-    # Sets Firefly / Renilla ratio for each variant
-    # Calculates the fold change for each variant
-    def calculate_fold_changes
-        self.luciferase_values.each do |lv|
-            lv.ff_ren_ratio = lv.firefly/lv.renilla
-            lv.fold_change = lv.ff_ren_ratio/self.ff_ren_ratio
-        end 
-    end
-    
-    def set_ratios_and_fold_changes
-        self.calculate_ff_ren_ratio
-        self.calculate_fold_changes
-    end 
 end
