0d9b68f00a02b4741167a6f6ab2bfb2678f0190d
Justin <justinms@outlook.com>
2021-01-22 14:27:49 -0600

    include variant id

--- stat3_luciferase_api/app/serializers/experiment_variant_serializer.rb
+++ stat3_luciferase_api/app/serializers/experiment_variant_serializer.rb
@@ -1,5 +1,5 @@
 class ExperimentVariantSerializer < ActiveModel::Serializer
-  attributes :protein_variant, :luciferase_values
+  attributes :id, :protein_variant, :luciferase_values
 
   def luciferase_values
     luciferase_value = object.luciferase_values.where(experiment_id: @instance_options[:experiment].id).map do |luciferase_value|
