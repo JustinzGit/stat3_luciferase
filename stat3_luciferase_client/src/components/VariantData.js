d639582e5e9713a603053ad13709a238bf9b880c
Justin <justinms@outlook.com>
2021-01-22 23:59:56 -0600

    use correct url param, change data labels

--- stat3_luciferase_client/src/components/VariantData.js
+++ stat3_luciferase_client/src/components/VariantData.js
@@ -1,24 +1,20 @@
 import React from "react"
 import { useHistory } from "react-router-dom"
-// import useExperiment from "../hooks/useExperiment"
 
-function VariantData({ data }){
-
-    // const { id } = useParams()
-    // const [experimentState, setExperimentState] = useExperiment(id)
+function VariantData({ data, variantName }){
     const history = useHistory()
 
     return(
         <div>
-            <p>Date: <b>{data.date}</b></p>
-            <p>WT Firefly: <b>{data.wt_firefly}</b></p>
-            <p>WT Renilla: <b>{data.wt_renilla}</b></p>
-            <p>WT FF/Ren: <b>{data.ff_ren_ratio}</b></p>
-            <p>MT Firefly: <b>{data.luciferase_values.firefly}</b></p>
-            <p>MT Renilla: <b>{data.luciferase_values.renilla}</b></p>
-            <p>MT FF/Ren: <b>{data.luciferase_values.ff_ren_ratio}</b></p>
+            <p>Experiment Date: <b>{data.date}</b></p>
+            <p>STAT3 Firefly: <b>{data.wt_firefly}</b></p>
+            <p>STAT3 Renilla: <b>{data.wt_renilla}</b></p>
+            <p>STAT3 FF/Ren: <b>{data.ff_ren_ratio}</b></p>
+            <p>{variantName} Firefly: <b>{data.luciferase_values.firefly}</b></p>
+            <p>{variantName} Renilla: <b>{data.luciferase_values.renilla}</b></p>
+            <p>{variantName} FF/Ren: <b>{data.luciferase_values.ff_ren_ratio}</b></p>
             <p>Fold Change <b>{data.luciferase_values.fold_change}</b></p>
-            <button onClick={() => history.push(`/experiments/edit/${data.date}`)}>Edit Experiment</button>
+            <button onClick={() => history.push(`/experiments/edit/${data.id}`)}>Edit Experiment</button>
             <hr></hr>
         </div>
     )
