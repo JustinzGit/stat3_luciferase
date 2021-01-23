90a0f54624efc9bd524028200da67e8fae37afd8
Justin <justinms@outlook.com>
2021-01-22 22:47:28 -0600

    create application contex, add app alerts to store

--- stat3_luciferase_client/src/App.js
+++ stat3_luciferase_client/src/App.js
@@ -1,35 +1,44 @@
 import { Switch, Route } from 'react-router-dom'
-
+import React, { useState }  from 'react'
 import Experiment from './components/Experiment'
 import ExperimentForm from './components/ExperimentForm'
 import VariantTable from './components/VariantTable'
 import Variant from './components/Variant'
 
+const AppContext = React.createContext({})
 
 function App() {
+  const [alerts, setAlerts] = useState('')
+
+  const store = {
+    alerts: { get: alerts, set: setAlerts }
+  }
+
   return (
-    <Switch>
-      <Route path={'/experiments/add'}>
-        <ExperimentForm />
-      </Route>
+    <AppContext.Provider value={store}>
+      <Switch>
+        <Route path={'/experiments/add'}>
+          <ExperimentForm />
+        </Route>
 
-      <Route path={'/experiments/edit/:id'}>
-        <ExperimentForm />
-      </Route>
+        <Route path={'/experiments/edit/:id'}>
+          <ExperimentForm />
+        </Route>
 
-      <Route path={'/experiments/:id'}>
-        <Experiment />
-      </Route>
+        <Route path={'/experiments/:id'}>
+          <Experiment />
+        </Route>
 
-      <Route path={'/variants/:protein_variant'}>
-        <Variant />
-      </Route>
+        <Route path={'/variants/:protein_variant'}>
+          <Variant />
+        </Route>
 
-      <Route path={'/variants'}>
-        <VariantTable />
-      </Route>
-    </Switch>
+        <Route path={'/variants'}>
+          <VariantTable />
+        </Route>
+      </Switch>
+    </AppContext.Provider>
   );
 }
 
-export default App;
+export { App, AppContext}
