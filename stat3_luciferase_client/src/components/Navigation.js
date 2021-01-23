2d68eab7d8ea95778fbc124c9d3c42d33f27ccdd
Justin <justinms@outlook.com>
2021-01-23 00:24:37 -0600

    assign variant id on form submission

--- stat3_luciferase_client/src/components/Navigation.js
+++ stat3_luciferase_client/src/components/Navigation.js
@@ -2,25 +2,36 @@ import React, { useState } from 'react';
 import { NavLink, useHistory } from 'react-router-dom'
 
 function Navigation({ variants, currentPath }) {
-    const [selectedVariant, setSelectedVariant] = useState("")
+    const [selectedVariant, setSelectedVariant] = useState('')
     const history = useHistory()
 
     function goToVariant(event){
         event.preventDefault()
-        history.push(`/variants/${selectedVariant}`)
+
+        // Store variant list in object for quick access to variant id
+        const variantObject = {}
+        variants.map(entry => {
+            return variantObject[entry.protein_variant] = entry
+        })
+
+        // If variant exists in database, navigate
+        if (variantObject[selectedVariant]){
+            const id = variantObject[selectedVariant].id
+            history.push(`/variants/${id}`)
+        }
     }
 
     return(
         <div id="navigation">
             {currentPath !== "/variants" && <NavLink to="/variants">Variant Table</NavLink>}
 
-            <form onSubmit={goToVariant}>
+            <p><form onSubmit={goToVariant}>
                 <input onChange={event => setSelectedVariant(event.target.value)} list="variants" id="variant-selection" autoComplete="off" />
                 <datalist id="variants">
-                    {variants.map((variant) => <option key={variant.id} value={variant.protein_variant}>{variant.protein_variant}</option>)}
+                    {variants.map((variant) => <option key={variant.id} value={variant.protein_variant}/>)}
                 </datalist>
                 <input type="submit" value="Go To Variant"/>
-            </form>
+            </form></p>
         </div>
     )
 }
